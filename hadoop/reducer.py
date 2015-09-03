ad89130062bdda5b101b3efa3eef5509e6a69018
manibhushan <manibhushan.cs@gmail.com>
2015-09-03 22:06:45 +0530

    reducer for forum merging

--- hadoop/reducer.py
+++ hadoop/reducer.py
@@ -5,5 +5,15 @@ import sys
 for line in sys.stdin:
     data = line.strip()
     values = data.split("\t")
-    for i in range(2):
-        print "VAL: ", values[i]
+    # for i in range(2):
+    #    print "VAL: ", values[i]
+    if values[0].isalpha():
+        continue
+    marker = values[1]
+    buff = None
+    if marker == "N":
+        buff = values[2:]
+    elif marker == "U":
+        buff.extend(values[2:])
+    else:
+        '\t'.join(buff)
