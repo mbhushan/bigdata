bbfce77cec03fe46882850a30e51392f72c926b7
manibhushan <manibhushan.cs@gmail.com>
2015-09-03 14:53:55 +0530

    fixed mapper for user and node forum type

--- hadoop/mapper.py
+++ hadoop/mapper.py
@@ -6,8 +6,8 @@ import csv
 
 def mapper():
     reader = csv.reader(sys.stdin, delimiter='\t')
-    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"',
-                        quoting=csv.QUOTE_ALL)
+    # writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"',
+    #                    quoting=csv.QUOTE_ALL)
 
     for line in reader:
         buf = list()
@@ -18,7 +18,7 @@ def mapper():
                 buf.append(line[i])
             buf.insert(0, "N")
             buf.insert(0, line[3])
-        if len(line) >= 5:
+        if len(line) <= 5:
             for i in range(1, 5):
                 buf.append(line[i])
             buf.insert(0, "U")
